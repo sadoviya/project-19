6d1294aad286b57a0bfa247bcdd3f7d5d6e5bc34
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-11-20 16:00:00 +0200

    add finaly slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import Swiper from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-new Swiper('.swiper');
+new Swiper('.swiper', {
+	modules: [Navigation, Pagination],
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
