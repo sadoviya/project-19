ee210a1382436f58562b584e7d449050718d2e89
Andrii Sadovyi <sadoviya@gmail.com>
2022-11-22 16:27:33 +0200

    fix

--- src/partials/js/_modal-add.js
+++ src/partials/js/_modal-add.js
@@ -4,19 +4,19 @@
         closeModalBtn: document.querySelector("[data-modal-add-close]"),
         modal: document.querySelector("[data-modal-add]"),
         body: document.querySelector('body'),
-        menuList: document.querySelector('.modal-list'),
+        // menuList: document.querySelector('.modal-list'),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-    refs.menuList.addEventListener('click', removeModal);
+    // refs.menuList.addEventListener('click', removeModal);
 
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
         refs.body.classList.toggle('no-scroll');
     }
     function removeModal() {
-        refs.menu.classList.add('is-hidden');
+        // refs.menu.classList.add('is-hidden');
         refs.body.classList.remove('no-scroll');
     }
 })();
