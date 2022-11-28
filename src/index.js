aab4d111f33795e8022f4885ba236de70c35fda6
Andrii Sadovyi <sadoviya@gmail.com>
2022-11-28 16:14:54 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './partials/js/_slider';
 // import './partials/js/_menu';
 // import './partials/js/_modal-send';
 // import './partials/js/_modal-add';
-// import './partials/js/_modal-buy';
+import './partials/js/_modal-buy';
 
